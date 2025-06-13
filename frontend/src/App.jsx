9ae355b27ebeac5a2a08cf8ba22a89c8ae645784
NickDimarzo <124927183+NickDimarzo@users.noreply.github.com>
2025-06-13 17:07:19 -0600

    added productStore from video

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className='text-red-500'>App</div>
-  )
+    <div className=" min-h-screen bg-base-200 transition-colors duration-300" data-theme="forest">
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/product/:id" element={<ProductPage />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
